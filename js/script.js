2a1c2745a59e6c63a7c673a4827b37bfa0d0f656
Toheeb Ashorobi <80055730+T-Ashorobi@users.noreply.github.com>
2021-08-13 16:22:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,16 +13,16 @@ For assistance:
 
 
 
+
+
 /*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+Create a 'showPage' function and its purpose is to create a set of pages with 9 pieces of information we will input from the data.js. We will create an HTML structure and loop through the data in certain parts of our structure and then append it.   
 */
 
 function showPage(list, page) {
    const startIndex = (page * 9) - 9 + 1;
    const endIndex = page * 9;
    const studentList = document.getElementsByClassName('student-list')[0];
-   // studentList.insertAdjacentHTML('beforeend', attach);
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i <= endIndex) {
@@ -46,55 +46,35 @@ showPage(data, 1);
 
 
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+
+
+/* 
+Create an 'addPagination' function and its purpose is to create a pagination list that is set to the amount of data we want show on a page (9) and that'll react to a 'click' eventListener. Once clicked the page number will correspond to the next list of 9.   
 */
 
-
-
 function addPagination(list) {
-   // create a variable to calculate the number of pages needed
    const pages = Math.ceil(list.length / 9);
-   // select the element with a class of `link-list` and assign it to a variable
    const linkList = document.querySelector('.link-list');
-   // set the innerHTML property of the variable you just created to an empty string
    linkList.innerHTML = '';
-   // loop over the number of pages needed
-   // create the elements needed to display the pagination button
-   // insert the above elements
-   // give the first pagination button a class of "active"
    for (let i = 1; i <= pages; i++) {
       linkList.insertAdjacentHTML('beforeend', `<li>
       <button type="button">${i}</button>
       </li>`);
       const firstButton = document.getElementsByTagName('button')[0];
-      //firstButton.className = 'active';
       firstButton.setAttribute('class', 'active');
-      // console.log(firstButton);
    };
 
-   // create an event listener on the `link-list` element
    linkList.addEventListener('click', (e) => {
       if (e.target.tagName === 'BUTTON') {
          const first = document.querySelector('.active');
          first.className = '';
-         // // firstButton.classList.remove('active');
-         const newButton = e.target.textContent;
+         const newButton = e.target;
          newButton.className = 'active';
-         // newButton.textContent = '1'; <- 1's start to override the other pagination numbers
-         // // firstEle.className = '';
-         // // console.log(firstEle);
-         // // newButton.className = 'active';
-         showPage(list, newButton);
+         showPage(list, newButton.textContent);
       }
    });
-   // if the click target is a button:
-   // remove the "active" class from the previous button
-   // add the active class to the clicked button
-   // call the showPage function passing the `list` parameter and page to display as arguments
 }
-showPage(data, 1);
-addPagination(data);
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
